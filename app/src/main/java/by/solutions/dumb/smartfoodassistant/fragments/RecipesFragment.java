8b339686f7d91a4b1c1eeb18286fccc7ae7b1411
DenisBelobrotski <belobrotskiy@gmail.com>
2017-11-17 02:48:02 +0300

    Union of navigation bar and fragments.

---
+++ app/src/main/java/by/solutions/dumb/smartfoodassistant/fragments/RecipesFragment.java
@@ -0,0 +1,64 @@
+package by.solutions.dumb.smartfoodassistant.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.solutions.dumb.smartfoodassistant.R;
+import by.solutions.dumb.smartfoodassistant.adapters.ProductAdapter;
+import by.solutions.dumb.smartfoodassistant.adapters.RecipeAdapter;
+import by.solutions.dumb.smartfoodassistant.containers.Product;
+import by.solutions.dumb.smartfoodassistant.containers.Recipe;
+
+
+public class RecipesFragment extends Fragment {
+
+    //region Variables
+
+    private List<Recipe> recipes = new ArrayList<>();
+    private ListView recipesList;
+
+    //endregion
+
+
+    //region Fragment lifecycle
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        testInitial();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recipes_fragment, container, false);
+
+        recipesList = view.findViewById(R.id.recipes_list);
+        RecipeAdapter recipeAdapter = new RecipeAdapter(getActivity().getApplicationContext(), R.layout.recipe, recipes);
+        recipesList.setAdapter(recipeAdapter);
+
+        return view;
+    }
+
+    //endregion
+
+
+    //region Private methods
+
+    private void testInitial() {
+        for (int i = 0; i < 5; i++) {
+            recipes.add(new Recipe("Жареная картоха"));
+            recipes.add(new Recipe("Вареный картофель"));
+            recipes.add(new Recipe("Тушеная картошка"));
+        }
+    }
+
+    //endregion
+}
